6eab3439069ff890e196d39f6e56f39a361c0f56
Henriquehnnm <henriquesardinha987@gmail.com>
2025-08-31 10:45:09 -0300

    Update

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -4,6 +4,13 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    -- Primeiro, carregue o tema de cores
     vim.cmd("colorscheme monokai-pro")
+
+    -- Habilita o destaque da linha do cursor
+    vim.opt.cursorline = true
+
+    -- Remove o destaque do texto da linha, mantendo-o apenas no gutter
+    vim.cmd("highlight CursorLine guibg=NONE")
   end,
 }
